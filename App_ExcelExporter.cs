90872e0766ebd966d138e7c1f52849802e251555
yang8320-dot <yang8320@gmail.com>
2026-04-24 16:22:25 +0800

    Update App_ExcelExporter.cs

--- App_ExcelExporter.cs
+++ App_ExcelExporter.cs
@@ -1,7 +1,3 @@
-/*
- * 檔案功能：匯出資料至 Excel，設定自訂表頭與藍色底線超連結。
- * 對應選單名稱：無
- */
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -19,7 +15,6 @@ namespace FormCrawlerApp
                 {
                     var worksheet = workbook.Worksheets.Add("表單資料");
 
-                    // 【需求 1】設定第一列專屬名稱
                     string[] headers = new string[] { "表單單號", "表單主題", "狀態", "狀態", "申請者", "承辦人", "目前處理者", "申請時間", "網址" };
                     for (int i = 0; i < headers.Length; i++)
                     {
@@ -28,18 +23,16 @@ namespace FormCrawlerApp
                         worksheet.Cell(1, i + 1).Style.Fill.BackgroundColor = XLColor.LightGray;
                     }
 
-                    // 寫入資料
                     int rowIdx = 2;
                     foreach (var row in dataList)
                     {
                         for (int colIdx = 0; colIdx < row.Length; colIdx++)
                         {
-                            if (colIdx == 8) // 第9欄是網址欄位
+                            if (colIdx == 8) 
                             {
                                 string link = row[colIdx];
                                 if (!string.IsNullOrWhiteSpace(link))
                                 {
-                                    // 【需求 3】以文字「超連結」顯示，並設定真實連結
                                     worksheet.Cell(rowIdx, 9).Value = "超連結";
                                     worksheet.Cell(rowIdx, 9).SetHyperlink(new XLHyperlink(link));
                                     worksheet.Cell(rowIdx, 9).Style.Font.FontColor = XLColor.Blue;
@@ -54,16 +47,10 @@ namespace FormCrawlerApp
                         rowIdx++;
                     }
 
-                    // 【修改點】設定有資料的列高為 25
+                    [span_3](start_span)// 設定 Excel 列高與自動調整欄寬[span_3](end_span)
                     worksheet.Rows().Height = 25;
-
-                    // 【修改點】依序設定 1~9 欄的欄寬
-                    double[] colWidths = { 50, 65, 9, 9, 9, 15, 15, 15, 10 };
-                    for (int i = 0; i < colWidths.Length; i++)
-                    {
-                        worksheet.Column(i + 1).Width = colWidths[i];
-                    }
-
+                    worksheet.Columns().AdjustToContents();
+                    
                     workbook.SaveAs(filePath);
                 }
             });
