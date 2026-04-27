454ce5f474b9f5a5c068b6ba45576b611fa4c43a
yang8320-dot <yang8320@gmail.com>
2026-04-27 20:19:52 +0800

    Update DbSettingsForm.cs

--- DbSettingsForm.cs
+++ DbSettingsForm.cs
@@ -145,9 +145,9 @@ namespace FormCrawlerApp
 
             // 【修改點】3. 微調自訂字的欄位間距，不要遮蔽
             Label lblCustom = new Label { Text = "[自訂字] 寫入：", Location = new Point(15, my + 4), AutoSize = true, ForeColor = Color.DarkGreen };
-            TextBox txtCustom = new TextBox { Location = new Point(170, my), Width = 200, Text = config.CustomTextValue };
-            Label lblArrow = new Label { Text = "➔", Location = new Point(380, my + 4), AutoSize = true, ForeColor = Color.DarkGray };
-            ComboBox cmbCustom = new ComboBox { Location = new Point(385, my), Width = 200, DropDownStyle = ComboBoxStyle.DropDownList };
+            TextBox txtCustom = new TextBox { Location = new Point(160, my), Width = 190, Text = config.CustomTextValue };
+            Label lblArrow = new Label { Text = "➔", Location = new Point(365, my + 4), AutoSize = true, ForeColor = Color.DarkGray };
+            ComboBox cmbCustom = new ComboBox { Location = new Point(380, my), Width = 180, DropDownStyle = ComboBoxStyle.DropDownList };
             
             cbCustom = cmbCustom;
             var existCustomMap = config.Mappings.FirstOrDefault(m => m.ScrapedField == customTextFieldName);
